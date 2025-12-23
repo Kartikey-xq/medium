2a6ffdd8f0d21860c0ec6d01d5c2393ddc0cf069
Kartikey-xq <joshikartikey32@gmail.com>
2025-12-23 13:18:34 +0530

    optimised prisma client for robust local/prod environment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,6 @@ import upload from './routers/upload.js';
 
 import {cors} from 'hono/cors';
 
-
 const app = new Hono<{
   Bindings: {        // Environment variables/bindings
     JWT_SECRET: string;
@@ -18,37 +17,16 @@ const app = new Hono<{
 app.use(
   '*',
   cors({
-    origin: (origin) => {
-      // allow dev + prod
-      const allowed = ['http://localhost:5173', 'medium-lyart-ten.vercel.app']
-      return allowed.includes(origin) ? origin : ''
-    },
-    allowHeaders: ['Content-Type', 'Authorization'],
-    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,  // very important
-  })
-)
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://medium-lyart-ten.vercel.app',
-  'https://medium-git-main-kartikeys-projects-2fe7d9d3.vercel.app'
-];
-
-
-app.use(
-  '*',
-  cors({
-    origin: (origin) => {
-      if (!origin) return ''; // no origin (like curl requests)
-      
+    origin: (origin: string | undefined) => {
+      if (!origin) return null; // no origin (like curl requests) omit the header, no browser in the picture, so no CORS implications
+      const allowed = origin === 'http://localhost:5173' || origin.endsWith('.vercel.app');
       // Always allow localhost for dev
-      if (origin === 'http://localhost:5173') return origin;
-
       // Allow any Vercel preview or production deployment
-      if (origin.endsWith('.vercel.app')) return origin;
+      if (allowed) return origin;
 
-      // Otherwise block
-      return '';
+
+      // Otherwise block as browser will enforce CORS
+      return null;
     },
     allowHeaders: ['Content-Type', 'Authorization'],
     allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -56,8 +34,8 @@ app.use(
   })
 );
 
-app.route("api/v1/user", user);
-app.route("api/v1/blog", blog);
-app.route("api/v1/upload", upload);
+app.route("/api/v1/user", user);
+app.route("/api/v1/blog", blog);
+app.route("/api/v1/upload", upload);
 
 export default app
--- backend/src/prisma-helper.ts
+++ backend/src/prisma-helper.ts
@@ -2,6 +2,10 @@ import { PrismaClient } from '@prisma/client/edge';
 import {withAccelerate} from '@prisma/extension-accelerate';
 
 export function getPrisma(databaseUrl: string) {
-
+    var url = databaseUrl ?? process.env.DATABASE_URL;
+    if(!url){
+        throw new Error("DATABASE_URL is not defined");
+    }
+    databaseUrl = url;
     return new PrismaClient({ datasourceUrl: databaseUrl }).$extends(withAccelerate())
 }
